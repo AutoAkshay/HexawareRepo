acd2cb6c4f57481d7db6505b5b21beed1157f2bb
AutoAkshay <akshaykalam93@gmail.com>
2026-01-11 15:20:05 +0530

    Radio Button

--- CodeStudioAutomation-master/CodeStudioAutomation-master/Automation/src/test/java/session1_9/Session9_radioButtonCount.java
+++ CodeStudioAutomation-master/CodeStudioAutomation-master/Automation/src/test/java/session1_9/Session9_radioButtonCount.java
@@ -8,10 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class Session9_radioButtonCount {
 	
 	public static void main(String[] args) {
-		
-//		System.setProperty("webdriver.driver.com", "C:\\Software\\chromedriver-win64\\chromedriver.exe");
-//		ChromeOptions options=new ChromeOptions();
-//		options.setBinary("C:\\Software\\chromedriver-win64\\chrome-win64\\chrome.exe");
+
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		
